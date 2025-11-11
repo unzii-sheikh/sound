5483493905981f59fda6df5ba9ed65fcaadc0f75
unzila hussain <unzilahussain8@gmail.com>
2025-11-12 00:59:46 +0500

    file created

---
+++ routes/web.php
@@ -0,0 +1,34 @@
+<?php
+
+use App\Http\Middleware\adminmiddleware;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+//admin middleware
+route::middleware([adminmiddleware::class])->group(function(){
+    //admin routes
+Route::get('/dashboard', function () {
+    return view('dashboard');
+});
+
+    
+});
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+    Route::get('/dashboard', function () {
+        if(Auth::user()->role == "admin"){
+
+            return view('dashboard');
+        }
+        else{
+            return redirect("/");
+        }
+    })->name('dashboard');
+});
